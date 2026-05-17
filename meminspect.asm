939b0c327c7ef35fa73a79c7bcd4b9f4c1ea21d4
Abishek Chaulagain <abishekchaulagain5225@gmail.com>
2026-05-17 22:54:49 +0545

    updated

--- meminspect.asm
+++ meminspect.asm
@@ -1,6 +1,8 @@
+default rel
+
 section .data
-    number dq 12345678
-    text db "Hello", 0
+    number dq 12345678 ;this creates memory variable| dq-> define quadword 
+    text db "Hello", 0 ;bd-> definebytes. This stores characters in memory
 
 section .text
     global _start
@@ -11,7 +13,7 @@ _start:
     mov rax, number
 
     ; Value at address
-    mov rbx, [number]
+    mov rbx, [number] ;the bracket [] indicates go to the memory location and read the values
 
     ; Exit program
     mov rax, 60
